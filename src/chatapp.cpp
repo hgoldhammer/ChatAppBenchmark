ba4151d12119d2409745f29e3bfee997d2934f70
Hauke Goldhammer <hauke.goldhammer@haw-hamburg.de>
2020-04-07 18:45:45 +0200

    add client and update chat

--- src/chatapp.cpp
+++ src/chatapp.cpp
@@ -25,6 +25,10 @@ using stop_atom = caf::atom_constant<caf::atom("stop")>;
 using join_atom = caf::atom_constant<caf::atom("join")>;
 using leave_atom = caf::atom_constant<caf::atom("leave")>;
 using left_atom = caf::atom_constant<caf::atom("left")>;
+using befriend_atom = caf::atom_constant<caf::atom("befriend")>;
+using logout_atom = caf::atom_constant<caf::atom("logout")>;
+using invite_atom = caf::atom_constant<caf::atom("invite")>;
+using act_atom = caf::atom_constant<caf::atom("act")>;
 
 /// simulates extern client events for each turn
 struct behavior_factory {
@@ -66,6 +70,25 @@ typename Inspector::result_type inspect(Inspector& f, behavior_factory& x) {
            x.invite);
 }
 
+/// for clients compute turn
+std::uint64_t fibonacci(std::uint8_t x) {
+  if (x == 0 || x == 1) {
+    return x;
+  } else {
+    auto j = x / 2;
+    auto fib_j = fibonacci(j);
+    auto fib_i = fibonacci(j - 1);
+
+    if (x % 2 == 0) {
+      return fib_j * (fib_j + (fib_i * 2));
+    } else if (x % 4 == 1) {
+      return (((fib_j * 2) + fib_i) * ((fib_j * 2) - fib_i)) + 2;
+    } else {
+      return (((fib_j * 2) + fib_i) * ((fib_j * 2) - fib_i)) - 2;
+    }
+  }
+}
+
 struct chat_state {
   client_set members;
   std::vector<std::vector<std::uint8_t>> buffer;
@@ -76,7 +99,7 @@ caf::behavior chat(caf::stateful_actor<chat_state>* self,
   self->state.members.insert(initiator);
   return {
     [=](post_atom, std::vector<std::uint8_t>& payload,
-        caf::actor& accumulator) {
+        const caf::actor& accumulator) {
       auto& s = self->state;
 #ifndef BENCH_NO_BUFFERED_CHATS
       s.buffer.push_back(payload);
@@ -90,7 +113,7 @@ caf::behavior chat(caf::stateful_actor<chat_state>* self,
         }
       }
     },
-    [=](join_atom, caf::actor& client, caf::actor& accumulator) {
+    [=](join_atom, const caf::actor& client, const caf::actor& accumulator) {
       auto& s = self->state;
       s.members.insert(client);
 #ifdef BENCH_NO_BUFFERED_CHATS
@@ -106,10 +129,115 @@ caf::behavior chat(caf::stateful_actor<chat_state>* self,
       }
 #endif
     },
-    [=](leave_atom, caf::actor& client, bool did_logout,
-        caf::actor& accumulator) {
+    [=](leave_atom, const caf::actor& client, const bool did_logout,
+        const caf::actor& accumulator) {
       self->state.members.erase(client);
       self->send(client, left_atom::value, self, did_logout, accumulator);
+    },
+    [=](leave_atom, const caf::actor& client, const bool did_logout) {
+      self->state.members.erase(client);
+      self->send(client, left_atom::value, self, did_logout);
+    },
+  };
+}
+
+struct client_state {
+  std::uint64_t id;
+  friend_set friends;
+  chat_set chats;
+  caf::actor& directory;
+  dice_roll dice;
+  pseudo_random rand;
+};
+
+caf::behavior client(caf::stateful_actor<client_state>* self,
+                     const std::uint64_t id, const caf::actor& directory,
+                     std::uint64_t seed) {
+  auto& s = self->state;
+  s.id = id;
+  s.directory = directory;
+  s.dice = dice_roll(seed);
+  s.rand = pseudo_random(seed);
+  return {
+    [=](befriend_atom, const caf::actor& client) {
+      self->state.friends.insert(client);
+    },
+    [=](logout_atom) {
+      auto& s = self->state;
+      if (s.chats.empty()) {
+        self->send(s.directory, left_atom::value, s.id);
+      } else {
+        for (auto& chat : s.chats) {
+          self->send(chat, leave_atom::value, self, true);
+        }
+      }
+    },
+    [=](left_atom, const caf::actor& chat, const bool did_logout,
+        const caf::actor& accumulator) {
+      auto& s = self->state;
+      s.chats.erase(chat);
+      self->send(accumulator, stop_atom::value);
+    },
+    [=](left_atom, const caf::actor& chat, const bool did_logout,
+        const caf::actor& accumulator) {
+      auto& s = self->state;
+      s.chats.erase(chat);
+      if (did_logout && s.chats.empty()) {
+        self->send(s.directory, left_atom::value, s.id);
+      }
+    },
+    [=](invite_atom, const caf::actor& chat, const caf::actor& accumulator) {
+      self->state.chats.insert(chat);
+      self->send(chat, join_atom::value, self, accumulator);
+    },
+    [=](forward_atom, const caf::actor& chat,
+        std::vector<std::uint8_t>& payload, const caf::actor& accumulator) {
+      self->send(accumulator, stop_atom::value);
+    },
+    [=](act_atom, behavior_factory& behavior, const caf::actor& accumulator) {
+      auto& s = self->state;
+      caf::actor next_chat;
+      if (s.chats.empty()) {
+        next_chat = self->spawn(chat, self);
+      } else {
+        size_t index = s.rand.next_int(s.chats.size());
+        next_chat = *std::next(s.chats.begin(), index);
+      }
+
+      switch (behavior.apply(s.dice)) {
+        case action::post:
+          self->send(next_chat, post_atom::value, std::vector<std::uint8_t>{},
+                     accumulator);
+          break;
+        case action::leave:
+          self->send(next_chat, leave_atom::value, false, accumulator);
+          break;
+        case action::compute:
+          fibonacci(35);
+          self->send(accumulator, stop_atom::value);
+        case action::none:
+          self->send(accumulator, stop_atom::value);
+          break;
+        case action::invite:
+          std::vector<caf::actor> f;
+          for (auto& i : s.friends) {
+            f.push_back(i);
+          }
+          auto rng = std::default_random_engine{s.rand.next_int()};
+          std::shuffle(f.begin(), f.end(), rng);
+          f.insert(f.begin(), self);
+
+          size_t invitations = s.rand.next_int(s.friends.size());
+          if (invitations == 0) {
+            self->send(accumulator, stop_atom::value);
+          } else {
+            self->send(accumulator, bump_atom::value, invitations);
+            for (size_t i = 0; i < invitations; ++i) {
+              self->send(f[i], invite_atom::value, next_chat, accumulator);
+            }
+          }
+          break;
+      }
     },
   };
 }
