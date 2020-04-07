abf113461b523f362e40ad388870e88b4ed88a5e
Hauke Goldhammer <hauke.goldhammer@haw-hamburg.de>
2020-04-07 16:48:49 +0200

    start chatapp, add chat actor

--- src/chatapp.cpp
+++ src/chatapp.cpp
@@ -1,1 +1,115 @@
- 
+#include <algorithm>
+#include <chrono>
+#include <vector>
+
+// util
+#include "dice_roll.hpp"
+
+// caf
+#include "caf/all.hpp"
+
+// types
+using client_map = std::unordered_map<std::uint64_t, caf::actor>;
+using friend_set = std::unordered_set<caf::actor>;
+using chat_set = std::unordered_set<caf::actor>;
+using client_set = std::unordered_set<caf::actor>;
+
+/// clients actions
+enum action { post, leave, invite, compute, none };
+
+// messages
+using post_atom = caf::atom_constant<caf::atom("post")>;
+using forward_atom = caf::atom_constant<caf::atom("forward")>;
+using bump_atom = caf::atom_constant<caf::atom("bump")>;
+using stop_atom = caf::atom_constant<caf::atom("stop")>;
+using join_atom = caf::atom_constant<caf::atom("join")>;
+using leave_atom = caf::atom_constant<caf::atom("leave")>;
+using left_atom = caf::atom_constant<caf::atom("left")>;
+
+/// simulates extern client events for each turn
+struct behavior_factory {
+  behavior_factory() = default;
+
+  behavior_factory(std::uint64_t compute, std::uint64_t post,
+                   std::uint64_t leave, std::uint64_t invite)
+    : compute(compute), post(post), leave(leave), invite(invite) {
+    // nop
+  }
+
+  behavior_factory(const behavior_factory& f) = default;
+
+  action apply(dice_roll dice) {
+    auto next_action = action::none;
+
+    if (dice.apply(compute)) {
+      next_action = action::compute;
+    } else if (dice.apply(post)) {
+      next_action = action::post;
+    } else if (dice.apply(leave)) {
+      next_action = action::leave;
+    } else if (dice.apply(invite)) {
+      next_action = action::invite;
+    }
+
+    return next_action;
+  }
+
+  std::uint64_t compute;
+  std::uint64_t post;
+  std::uint64_t leave;
+  std::uint64_t invite;
+};
+
+template <class Inspector>
+typename Inspector::result_type inspect(Inspector& f, behavior_factory& x) {
+  return f(caf::meta::type_name("behavior_factory"), x.compute, x.post, x.leave,
+           x.invite);
+}
+
+struct chat_state {
+  client_set members;
+  std::vector<std::vector<std::uint8_t>> buffer;
+};
+
+caf::behavior chat(caf::stateful_actor<chat_state>* self,
+                   const caf::actor& initiator) {
+  self->state.members.insert(initiator);
+  return {
+    [=](post_atom, std::vector<std::uint8_t>& payload,
+        caf::actor& accumulator) {
+      auto& s = self->state;
+#ifndef BENCH_NO_BUFFERED_CHATS
+      s.buffer.push_back(payload);
+#endif
+      if (s.members.empty()) {
+        self->send(accumulator, stop_atom::value);
+      } else {
+        self->send(accumulator, bump_atom::value, s.members.size());
+        for (auto& member : s.members) {
+          self->send(member, forward_atom::value, self, payload, accumulator);
+        }
+      }
+    },
+    [=](join_atom, caf::actor& client, caf::actor& accumulator) {
+      auto& s = self->state;
+      s.members.insert(client);
+#ifdef BENCH_NO_BUFFERED_CHATS
+      self->send(accumulator, stop_atom::value);
+#else
+      if (s.buffer.empty()) {
+        self->send(accumulator, stop_atom::value);
+      } else {
+        self->send(accumulator, bump_atom::value, s.buffer.size());
+        for (auto& message : s.buffer) {
+          self->send(client, forward_atom::value, message, accumulator);
+        }
+      }
+#endif
+    },
+    [=](leave_atom, caf::actor& client, bool did_logout,
+        caf::actor& accumulator) {
+      self->state.members.erase(client);
+      self->send(client, left_atom::value, self, did_logout, accumulator);
+    },
+  };
+}
